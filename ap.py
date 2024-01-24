5d0d6c85b4c62a357a480b505ab1f51ef1b6f4dc
Yanik <user@MacBook-Pro.local>
2024-01-25 02:17:17 +0300

    Вот пусть, это и будет первым "настоящим" файлом с программой, которая будет совершенствоваться...#

--- ap.py
+++ ap.py
@@ -1,10 +1,37 @@
-print('lol')
-print('bugFix')
-for i in range(1,5):
-    print(i)
+import math as m
 
-print('kek')
-print('again')
+print('Если вы вводите три стороны a, b, c, нажмите \'1\' если стороны a, b и угол С, нажмите \'2\'')
+user_input = int(input())
 
-a = 12
-b = 21
+while True:
+    a, b, c = 0, 0, 0
+    cornerC = 0
+    R = 0
+    S = 0
+    if user_input == 1:
+        print('Введите a, b, c:')
+        a, b, c = [int(_) for _ in input().split()]
+        if a + b > c and a + c > b and c + b > a:
+            cornerC = m.acos((a ** 2 + b ** 2 - c ** 2) / (2 * a * b))
+        else:
+            print('Введите корректные значения a, b, c:')
+            continue
+
+    if user_input == 2:
+        print('Введите a, b и угол С')
+        a, b, cornerC = [int(_) for _ in input().split()]
+        if a > 0 and b > 0 and 0 < cornerC < 180:
+            print((a ** 2 + b ** 2 - (2 * a * b) * (m.cos(m.radians(cornerC)))))
+            print((a ** 2 + b ** 2 - (2 * a * b) * (m.cos(m.radians(cornerC)))) ** 0.5)
+            c = (a ** 2 + b ** 2 - (2 * a * b) * (m.cos(m.radians(cornerC)))) ** 0.5
+        else:
+            print('Введите корректные значения a, b, C:')
+            continue
+
+    R = (((a + b + c) / 2) - c) * m.tan(m.radians(cornerC) / 2)
+    S = m.pi * R ** 2
+    print(f'площадь вписанной окружности равна {S}')
+    break
+
+
+
